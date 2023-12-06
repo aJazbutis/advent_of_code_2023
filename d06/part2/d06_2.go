06703acd0c90daacbdd4458fbae84e2adf51da50
aJazbutis <arvydas.jazbutis@gmail.com>
2023-12-06 15:31:59 +0200

    finished: d06 part2

---
+++ d06/part2/d06_2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"advent/utils"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func linesToData(lines []string) (int, int) {
+	tmp := []string{}
+	for i := range lines {
+		tmp = append(tmp, strings.Split(lines[i], ":")[1])
+	}
+	tmp2 := [][]string{}
+	for i := range tmp {
+		tmp2 = append(tmp2, strings.Fields(tmp[i]))
+	}
+	t, d := "", ""
+	for i := range tmp2[0] {
+		t += tmp2[0][i]
+		d += tmp2[1][i]
+	}
+	time, _ := strconv.Atoi(t)
+	dist, _ := strconv.Atoi(d)
+	return time, dist
+}
+
+func ways(time int, record int) int {
+	ret := 0
+	speed := 0
+	for speed < time {
+		dist := speed * (time - speed)
+		if dist > record {
+			ret++
+		}
+		speed++
+	}
+	return ret
+}
+
+func main() {
+	defer utils.Panicked()
+	utils.CheckArgs(2, os.Args)
+	ret := ways(linesToData(utils.GetLinesFromFile(os.Args[1])))
+	fmt.Println(ret)
+}
